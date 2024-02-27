dd5f7d20afa2f429cb5cbc2534246f2d38fed981
Cahid <mr.cebizade@gmail.com>
2024-02-27 16:03:59 +0400

    hotfix

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ import uuid
 
 class BaseModel:
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
