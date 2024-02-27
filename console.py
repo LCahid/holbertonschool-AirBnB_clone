baa56cc0de4843ceb3ab5ef7df82e729fe89dd65
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-27 22:57:20 +0400

    console py

--- console.py
+++ console.py
@@ -7,16 +7,22 @@ class HBNBCommand(cmd.Cmd):
     '''Something more useful'''
     prompt = "(hbnb) "
 
-    def do_quit(self):
+    def do_quit(self, arg):
         '''Quit command to exit the program'''
         exit()
 
-    def do_EOF(self):
+    def do_EOF(self, arg):
         '''EOF command to exit the program'''
         exit()
 
     def emptyline(self):
         pass
+
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
+        elif not arg == "BaseModel":
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
         HBNBCommand().cmdloop()
