97970461b04546a6e9ca23de4de795a4c5bc295e
Cahid <mr.cebizade@gmail.com>
2024-02-27 18:38:00 +0400

    circular import problem fixed

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from datetime import datetime
 import uuid
-from models import storage
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
