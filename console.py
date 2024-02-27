cffcf5e2d631ea0358f03a1952349c07707169df
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-27 22:41:02 +0400

    console py

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Something usful'''
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''Something more useful'''
+    self.prompt = "(hbnb) "
+
+    def do_quit(self):
+        '''Quit command to exit the program'''
+        exit()
+
+    def do_EOF(self):
+        '''EOF command to exit the program'''
+        exit()
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+        HBNBCommand().cmdloop()
